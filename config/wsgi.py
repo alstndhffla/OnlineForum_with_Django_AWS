8129a0d74bcb297fa9a6feb492bbb7932da4f1e5
Minsoo Kim <alstndhffla@gmail.com>
2021-03-06 22:42:08 +0900

    modified

--- config/wsgi.py
+++ config/wsgi.py
@@ -5,6 +5,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+
+이 파일은 Gunicorn과 같은 WSGI서버에서 호출하는 장고의 WSGI 애플리케이션이다.
+WSGI 서버는 항상 이 파일을 호출하여 장고(django) 프로그램을 실행한다.
 """
 
 import os
